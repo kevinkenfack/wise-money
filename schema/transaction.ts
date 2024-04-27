b2741e276888d638b917518b3791b1610c8e1556
kevinkenfack <kenfackjoelkevin@yahoo.com>
2024-04-27 16:21:37 +0100

    update ui

--- schema/transaction.ts
+++ schema/transaction.ts
@@ -5,7 +5,7 @@ export const CreateTransactionSchema = z.object({
   description: z.string().optional(),
   date: z.coerce.date(),
   category: z.string(),
-  type: z.union([z.literal("Revenue"), z.literal("Depense")]),
+  type: z.union([z.literal("income"), z.literal("expense")]),
 });
 
 export type CreateTransactionSchemaType = z.infer<
