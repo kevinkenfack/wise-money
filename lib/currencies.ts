bb5ec7a8f9a8c52c92bfa97f09cfabaad57086c3
kevinkenfack <kenfackjoelkevin@yahoo.com>
2024-04-27 08:23:27 +0100

    update currency

--- lib/currencies.ts
+++ lib/currencies.ts
@@ -1,8 +1,8 @@
 export const Currencies = [
   { value: "USD", label: "$ Dollar", locale: "en-US" },
   { value: "EUR", label: "€ Euro", locale: "de-DE" },
-  { value: "JPY", label: "¥ Yen", locale: "ja-JP" },
-  { value: "GBP", label: "£ Pound", locale: "en-GB" },
+  // { value: "JPY", label: "¥ Yen", locale: "ja-JP" },
+  // { value: "GBP", label: "£ Pound", locale: "en-GB" },
   { value: "XAF", label: "Franc CFA d'Afrique centrale", locale: "fr-FR" }, // Ajouté XAF
   { value: "XOF", label: "Franc CFA d'Afrique de l'Ouest", locale: "fr-FR" }, // Ajouté XOF
 ];
