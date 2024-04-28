479bfef8ec368e38dced33574c8599bd9427e02e
kevinkenfack <kenfackjoelkevin@yahoo.com>
2024-04-28 10:55:35 +0100

    update UI

--- app/(dashboard)/_components/History.tsx
+++ app/(dashboard)/_components/History.tsx
@@ -45,7 +45,7 @@ function History({ userSettings }: { userSettings: UserSettings }) {
 
   return (
     <div className="container">
-      <h2 className="mt-12 text-3xl font-bold">Historique</h2>
+      <h2 className="mt-12 text-3xl font-bold">Analyse</h2>
       <Card className="col-span-12 mt-2 w-full">
         <CardHeader className="gap-2">
           <CardTitle className="grid grid-flow-row justify-between gap-2 md:grid-flow-col">
